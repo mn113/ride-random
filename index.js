73e0bf9a6799b37904d7a890149d3b37e3636753
Martin Nicholson <recyclebing+github@gmail.com>
2018-06-18 01:40:12 +0100

    Boilerplate files

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const fs = require('fs');
+const prettyjson = require('prettyjson')
+const express = require('express');
+const app = express();
 
+// Serve static files:
+app.use(express.static('public'));
+
+// Test:
+app.get('/hw', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server:
+app.listen(3003, () => {
+  console.log('Example app listening on port 3000!');
+});
+
+// Set up client:
 const gmapsApiKey = fs.readFileSync('gmaps-apikey.txt');
-
 var googleMapsClient = require('@google/maps').createClient({
   key: gmapsApiKey
 });
 
 // Geocode an address.
 googleMapsClient.geocode({
-  address: '1600 Amphitheatre Parkway, Mountain View, CA'
+  address: 'Bristol, UK'
 }, function(err, response) {
   if (!err) {
-    console.log(response.json.results);
+    console.log(prettyjson.render(response.json.results));
   }
 });
 
+var homeCoords = [51.454513,-2.58791];
 
+function genPoint(last, nextlast) {
+
+}
+
+function snapPoint(point) {
+
+}
+
+function toEndOfRoad(segment, point, direction) {
+
+}
+
+function calcDist(a, b) {
+
+}
