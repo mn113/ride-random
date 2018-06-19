7b4a196ee5ac3299b416f43d022b7dac090b56b7
Martin Nicholson <recyclebing+github@gmail.com>
2018-06-19 16:29:45 +0100

    Installed Awesomplete & GMap

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,3 +1,5 @@
+/* global Awesomplete, google */
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/js/worker.js', {scope: '/js/'})
         .then(reg => {
@@ -9,5 +11,41 @@ if ('serviceWorker' in navigator) {
         });
 }
 
-var startLoc,
-    endLoc;
+var startLoc = {};
+
+// Set up autocomplete:
+var input = document.querySelector("#start");
+var awesomplete = new Awesomplete(input);
+input.addEventListener('input', () => {
+    if (input.value.length < 3) return;
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", "/places/" + input.value, true);
+    ajax.onload = () => {
+        var jsonPlaces = JSON.parse(ajax.responseText);
+        console.log(jsonPlaces);
+        awesomplete.list = jsonPlaces.map(i => {
+            return {label: i.desc, value: i.pid};
+        });
+        awesomplete.evaluate();
+    };
+    ajax.send();
+});
+// Apply user's place selection:
+input.addEventListener('awesomplete-selectcomplete', (e) => {
+    console.log(e.text);
+    startLoc.pid = e.text.value;
+    input.value = e.text.label;
+    // Start ProcGen
+});
+
+function mapGoToPlace() {
+
+}
+
+var gmap;
+function initMap() {
+    gmap = new google.maps.Map(document.getElementById('gmap'), {
+        center: {lat: 51, lng: -3},
+        zoom: 3
+    });
+}
